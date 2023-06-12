496c73a26a437cb863214cb1194e0c6339aa174e
b3rt01ac3 <andre@bertolace.com>
2023-06-12 10:03:06 +0200

    adding "robust" CNN model

--- utils/torch/model.py
+++ utils/torch/model.py
@@ -27,3 +27,24 @@ class Ours(torch.nn.Linear):
         else:
             x = torch.hstack((torch.Tensor(x), torch.ones(1))) if self.const else x
             return (torch.sign(torch.matmul(x, self.w)) + 1) / 2
+
+
+class CNN(torch.nn.Module):
+    def __init__(self, xi: float = 0.0):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(3, 6, 5)
+        self.pool = torch.nn.MaxPool2d(2, 2)
+        self.conv2 = torch.nn.Conv2d(6, 16, 5)
+        self.fc1 = torch.nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.fc3 = torch.nn.Linear(84, 10)
+        self.xi = xi
+
+    def forward(self, x):
+        x = self.pool(torch.nn.functionalF.relu(self.conv1(x) - 2 * self.xi))
+        x = self.pool(torch.nn.functional.F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = torch.nn.functional.F.relu(self.fc1(x))
+        x = torch.nn.functional.F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x

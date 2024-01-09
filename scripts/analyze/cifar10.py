c7fd83f1304101d51987782999bf729458bcf24b
f2cf2e10 <andre@bertolace.com>
2024-01-09 10:40:05 +0100

    updating analysis scripts

--- scripts/analyze/cifar10.py
+++ scripts/analyze/cifar10.py
@@ -1,58 +1,63 @@
 import matplotlib
 import pandas as pd
 
-matplotlib.rc('axes', titlesize=22)
-matplotlib.rc('legend', fontsize=14)
-matplotlib.rc('axes', labelsize=22)
-matplotlib.rc('xtick', labelsize=22)
-matplotlib.rc('ytick', labelsize=22)
-
+matplotlib.rc('axes', titlesize=20)
+matplotlib.rc('legend', fontsize=22)
+matplotlib.rc('axes', labelsize=20)
+matplotlib.rc('xtick', labelsize=20)
+matplotlib.rc('ytick', labelsize=20)
 # cat/dog
-cifar_0_05 = pd.read_csv("./CIFAR10_catdog_0_05.csv", index_col=0, sep=";")[['Plain Test Acc', 'FGSM Test Acc']].loc[
-    ['Plain', 'FGSM', 'PGD', 'Ours']]
+cifar_0_05 = pd.read_csv("./CIFAR10_catdog_0_05.csv", index_col=0, sep=",")[['Plain Test Acc', 'FGSM Test Acc']].loc[
+    ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 cifar_0_05.columns = ['xi=0.0', 'xi=0.05']
-cifar_0_1 = pd.read_csv("./CIFAR10_catdog_0_1.csv", index_col=0, sep=";")[['FGSM Test Acc']].loc[
-    ['Plain', 'FGSM', 'PGD', 'Ours']]
+cifar_0_1 = pd.read_csv("./CIFAR10_catdog_0_1.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
+    ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 cifar_0_1.columns = ['xi=0.1']
-cifar_0_25 = pd.read_csv("./CIFAR10_catdog_0_25.csv", index_col=0, sep=";")[['FGSM Test Acc']].loc[
-    ['Plain', 'FGSM', 'PGD', 'Ours']]
-cifar_0_25.columns = ['xi=0.25']
+cifar_0_15 = pd.read_csv("./CIFAR10_catdog_0_15.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
+    ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
+cifar_0_15.columns = ['xi=0.1']
+cifar_0_2 = pd.read_csv("./CIFAR10_catdog_0_2.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
+    ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
+cifar_0_2.columns = ['xi=0.2']
 
-cifar_catdog = pd.concat([cifar_0_05, cifar_0_1, cifar_0_25], join='inner', axis=1)
-cifar_catdog.index = ['Training', 'FGSM', 'PGD', 'Ours']
+cifar_catdog = pd.concat([cifar_0_05, cifar_0_1, cifar_0_15, cifar_0_2], join='inner', axis=1)
+cifar_catdog.index = ['Training', 'FGSM', 'PGD', 'TRADES', 'Our Approach']
 ax = cifar_catdog.plot.bar(figsize=(12, 9), rot=0)
 ax.set_ylim(0, 100)
 ax.set_label('Training methodology')
 ax.set_ylabel('Accuracy [%]')
 ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=4)
 
-ax = cifar_catdog.T.plot(figsize=(12, 9), style='o--', linewidth=3, rot=0)
+ax = cifar_catdog.T.plot(figsize=(16, 10), style='o--', linewidth=3, rot=0)
 ax.set_ylim(0, 100)
 ax.set_label('xi')
 ax.set_ylabel('Accuracy [%]')
-ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=4)
+ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=5)
 
 # airplane/dog
-cifar_0_05 = pd.read_csv("./CIFAR10_airplanedog_0_05.csv", index_col=0, sep=";")[['Plain Test Acc', 'FGSM Test Acc']].loc[
-    ['Plain', 'FGSM', 'PGD', 'Ours']]
+cifar_0_05 = pd.read_csv("./CIFAR10_airplanedog_0_05.csv", index_col=0, sep=",")[['Plain Test Acc', 'FGSM Test Acc']].loc[
+    ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 cifar_0_05.columns = ['xi=0.0', 'xi=0.05']
-cifar_0_1 = pd.read_csv("./CIFAR10_airplanedog_0_1_.csv", index_col=0, sep=";")[['FGSM Test Acc']].loc[
-    ['Plain', 'FGSM', 'PGD', 'Ours']]
+cifar_0_1 = pd.read_csv("./CIFAR10_airplanedog_0_1.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
+    ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 cifar_0_1.columns = ['xi=0.1']
-cifar_0_25 = pd.read_csv("./CIFAR10_airplanedog_0_25.csv", index_col=0, sep=";")[['FGSM Test Acc']].loc[
-    ['Plain', 'FGSM', 'PGD', 'Ours']]
-cifar_0_25.columns = ['xi=0.25']
+cifar_0_15 = pd.read_csv("./CIFAR10_airplanedog_0_15.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
+    ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
+cifar_0_15.columns = ['xi=0.1']
+cifar_0_2 = pd.read_csv("./CIFAR10_airplanedog_0_2.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
+    ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
+cifar_0_2.columns = ['xi=0.25']
 
-cifar_airplanedog = pd.concat([cifar_0_05, cifar_0_1, cifar_0_25], join='inner', axis=1)
-cifar_airplanedog.index = ['Training', 'FGSM', 'PGD', 'Ours']
+cifar_airplanedog = pd.concat([cifar_0_05, cifar_0_1, cifar_0_15, cifar_0_2], join='inner', axis=1)
+cifar_airplanedog.index = ['Training', 'FGSM', 'PGD', 'TRADES', 'Our Approach']
 ax = cifar_airplanedog.plot.bar(figsize=(12, 9), rot=0)
 ax.set_ylim(0, 100)
 ax.set_label('Training methodology')
 ax.set_ylabel('Accuracy [%]')
 ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=4)
 
-ax = cifar_airplanedog.T.plot(figsize=(12, 9), style='o--', linewidth=3, rot=0)
+ax = cifar_airplanedog.T.plot(figsize=(16, 10), style='o--', linewidth=3, rot=0)
 ax.set_ylim(0, 100)
 ax.set_label('xi')
 ax.set_ylabel('Accuracy [%]')
-ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=4)
+ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=5)
--- scripts/analyze/nist.py
+++ scripts/analyze/nist.py
@@ -1,64 +1,64 @@
 import pandas as pd
 import matplotlib
 
-matplotlib.rc('axes', titlesize=25)
-matplotlib.rc('legend', fontsize=25)
-matplotlib.rc('axes', labelsize=22)
-matplotlib.rc('xtick', labelsize=22)
-matplotlib.rc('ytick', labelsize=22)
+matplotlib.rc('axes', titlesize=20)
+matplotlib.rc('legend', fontsize=22)
+matplotlib.rc('axes', labelsize=20)
+matplotlib.rc('xtick', labelsize=20)
+matplotlib.rc('ytick', labelsize=20)
 
 # 0/1
-nist_0_05 = pd.read_csv("./NIST_01_0_05.csv", index_col=0, sep=";")[['Plain Test Acc', 'FGSM Test Acc']].loc[
+nist_0_05 = pd.read_csv("./NIST_01_0_05.csv", index_col=0, sep=",")[['Plain Test Acc', 'FGSM Test Acc']].loc[
     ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 nist_0_05.columns = ['xi=0.0', 'xi=0.05']
-nist_0_1 = pd.read_csv("./NIST_01_0_1.csv", index_col=0, sep=";")[['FGSM Test Acc']].loc[
+nist_0_1 = pd.read_csv("./NIST_01_0_1.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
     ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 nist_0_1.columns = ['xi=0.1']
-nist_0_15 = pd.read_csv("./NIST_01_0_15.csv", index_col=0, sep=";")[['FGSM Test Acc']].loc[
+nist_0_15 = pd.read_csv("./NIST_01_0_15.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
     ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 nist_0_15.columns = ['xi=0.15']
-nist_0_2 = pd.read_csv("./NIST_01_0_2.csv", index_col=0, sep=";")[['FGSM Test Acc']].loc[
+nist_0_2 = pd.read_csv("./NIST_01_0_2.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
     ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 nist_0_2.columns = ['xi=0.2']
 
 nist_01 = pd.concat([nist_0_05, nist_0_1, nist_0_15, nist_0_2], join='inner', axis=1)
-nist_01.index = ['Training', 'FGSM', 'PGD', 'TRADES [1.0]', 'Proposed Approach']
+nist_01.index = ['Training', 'FGSM', 'PGD', 'TRADES', 'Our Approach']
 ax = nist_01.plot.bar(figsize=(12, 9), rot=0)
 ax.set_ylim(0, 100)
 ax.set_label('Training methodology')
 ax.set_ylabel('Accuracy [%]')
-ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=4)
+ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=5)
 
-ax = nist_01.T.plot(figsize=(12, 9), style='o--', linewidth=3, rot=0)
+ax = nist_01.T.plot(figsize=(16, 10), style='o--', linewidth=3, rot=0)
 ax.set_ylim(0, 100)
 ax.set_label('xi')
 ax.set_ylabel('Accuracy [%]')
-ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=4)
+ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=5)
 
 # 3/8
-nist_0_05 = pd.read_csv("./NIST_38_0_05.csv", index_col=0, sep=";")[['Plain Test Acc', 'FGSM Test Acc']].loc[
+nist_0_05 = pd.read_csv("./NIST_38_0_05.csv", index_col=0, sep=",")[['Plain Test Acc', 'FGSM Test Acc']].loc[
     ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 nist_0_05.columns = ['xi=0.0', 'xi=0.05']
-nist_0_1 = pd.read_csv("./NIST_38_0_1.csv", index_col=0, sep=";")[['FGSM Test Acc']].loc[
+nist_0_1 = pd.read_csv("./NIST_38_0_1.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
     ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 nist_0_1.columns = ['xi=0.1']
-nist_0_15 = pd.read_csv("./NIST_38_0_15.csv", index_col=0, sep=";")[['FGSM Test Acc']].loc[
+nist_0_15 = pd.read_csv("./NIST_38_0_15.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
     ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 nist_0_15.columns = ['xi=0.15']
-nist_0_2 = pd.read_csv("./NIST_38_0_2.csv", index_col=0, sep=";")[['FGSM Test Acc']].loc[
+nist_0_2 = pd.read_csv("./NIST_38_0_2.csv", index_col=0, sep=",")[['FGSM Test Acc']].loc[
     ['Plain', 'FGSM', 'PGD', 'TRADES', 'Ours']]
 nist_0_2.columns = ['xi=0.2']
 
 nist_38 = pd.concat([nist_0_05, nist_0_1, nist_0_15, nist_0_2], join='inner', axis=1)
-nist_38.index = ['Training', 'FGSM', 'PGD', 'TRADES [1.0]', 'Proposed Approach']
+nist_38.index = ['Training', 'FGSM', 'PGD', 'TRADES', 'Our Approach']
 ax = nist_38.plot.bar(figsize=(12, 9), rot=0)
 ax.set_ylim(0, 100)
 ax.set_label('Training methodology')
 ax.set_ylabel('Accuracy [%]')
-ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=4)
+ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=5)
 
-ax = nist_38.T.plot(figsize=(12, 9), style='o--', linewidth=3, rot=0)
+ax = nist_38.T.plot(figsize=(16, 10), style='o--', linewidth=3, rot=0)
 ax.set_ylim(0, 100)
 ax.set_label('xi')
 ax.set_ylabel('Accuracy [%]')
-ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=4)
+ax.legend(frameon=False, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=5)

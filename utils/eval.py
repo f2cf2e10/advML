3119fade59daf7079f6f0bcac018442166605714
f2cf2e10 <andre@bertolace.com>
2024-01-20 14:59:50 +0100

    Saving models for later use

--- utils/eval.py
+++ utils/eval.py
@@ -7,11 +7,11 @@ import scipy as sp
 def RoMa(delta, xi, x_y, n, model):
     x = x_y[0]
     y = 2 * x_y[1] - 1
-    x_0 = nn.Flatten(1, x.dim() - 1)(x)
+    x_0 = nn.Flatten(0, x.dim() - 1)(x)
     hic = np.zeros(n)
     for i in range(n):
         x_i = torch.clamp(x_0 + torch.FloatTensor(x_0.shape).uniform_(-xi, xi), -1, 1)
-        y_i = model(x_i).detach().numpy()[0][0]
+        y_i = model(x_i).detach().numpy()[0]
         prob = 1. / (1 + np.exp(-np.abs(y_i)))
         hic[i] = 1 - prob if np.sign(y) == np.sign(y_i) else prob
     box_cox = sp.stats.boxcox([h for h in hic if h > 0.0])
